006715680a5b7eb3ce89dfc8ec9c28b7b8121c40
nyix <rsa17826@email.vccs.edu>
2026-08-04 11:15:22 -0400

    NO MESSAGE SET

--- _room_geometry.py
+++ _room_geometry.py
@@ -6466,7 +6466,7 @@ WARPS: tuple[Warp, ...] = (
     "connections": ((4, 13, "root", 0), (100, 100, "south", 0)),
   },
   {
-    "reqs": [["magic:lightning", "area:magic upgrades", "skill:revela"]],
+    "reqs": [["magic:lightning", "area:magic upgrades", "skill:reveal"]],
     "connections": ((16, 11, "root", 0), (17, 10, "east", 0)),
   },
   {
